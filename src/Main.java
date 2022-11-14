398e8c5e79be97a8fadfcbfc07465b2620ac7841
Lenar Khafizov <lenarkh@mail.ru>
2022-11-14 23:11:34 +0300

    Первый вариант программы с ошибкой в подсчете символов

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
+        public static String textInp() {
+        return "Доброй ночи!"; // Ввод исходного текста
+        }
+
     public static void main(String[] args) {
         System.out.println("Домашнее задание. Коллекции HashSet и HashMap.");
+        String text = textInp();
+        Map<Character, Integer> textMap = new HashMap<>();
+        textMap.put(text.charAt(0), 1);
+        for (int i = 1; i < text.length(); i++) {
+            char s = text.charAt(i);
+            for (Character key:textMap.keySet()) {
+                if (s == key) {
+                    int sAmount = textMap.get(s);
+                    sAmount++;
+                    textMap.put(s,sAmount);
+                    break;
+                }
+                else {
+                    textMap.put(s, 1);
+                }
+           }
+        }
+        System.out.println(textMap);
     }
 }
